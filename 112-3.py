210fe71d8637ebc408627815cd8b8fe03d1f8691
lena <lny970116@gmail.com>
2021-05-13 18:45:52 +0900

    mod 112

--- 112-3.py
+++ 112-3.py
@@ -1,9 +1,6 @@
+#이전 것도 맞았지만, 더 깔끔하게 코드 정리
 pin='881120-1068234'
-print('yyyymmdd= 19'+pin[0:6])
-print('num= '+pin[7:])
-
-'''
-출력 결과
-yyyymmdd= 19881120
-num= 1068234
-'''
+yyyymmdd=pin[:6]
+num=pin[7:]
+print(yyyymmdd) #881120
+print(num) #1068234
--- 112-5.py
+++ 112-5.py
@@ -1,2 +1,4 @@
+#이전 것도 맞았지만, 더 깔끔하게 코드 정리
 a='a:b:c:d'
-print(a.replace(':','#')) # a#b#c#d
+b=a.replace(':','#')
+print(b) #  a#b#c#d
